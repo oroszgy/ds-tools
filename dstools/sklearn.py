261b6d25ef057045f94c6e7921d2737592a42de5
Gyorgy Orosz <oroszgy@gmail.com>
2017-05-24 11:25:45 +0200

    Added top_k prediction method

--- dstools/sklearn.py
+++ dstools/sklearn.py
@@ -1,6 +1,16 @@
 from functools import partial
 
+import numpy
 from sklearn.preprocessing import FunctionTransformer
+
+
+def predict_k(pipeline, X, k=3):
+    X = pipeline.transform(X)
+    y_scores = pipeline.predict_proba(X)
+    labels = pipeline.steps[-1][1].classes_
+    result = [[(str(l), float(s)) for l, s in zip(labels[numpy.array(scores.argsort())], numpy.sort(scores))][-k:][::-1]
+              for scores in y_scores]
+    return result
 
 
 def _batch_process(texts, func):
