94a0456e21d2835f690e5fcbafc1c820bd460412
Gyorgy Orosz <oroszgy@gmail.com>
2017-06-15 22:31:36 +0200

    Refactored experimetn tracker

--- mltoolz/experiments.py
+++ mltoolz/experiments.py
@@ -8,24 +8,28 @@ class ExperimentTracker(object):
     def __init__(self, pkl_path, append=True):
         self._fpath = Path(pkl_path)
         if self._fpath.exists() and append:
-            self.data = pd.read_pickle(self._fpath.open())
+            self.data = pd.read_pickle(str(self._fpath))
         else:
             self.data = pd.DataFrame()
+
+    def _store(self, result):
+        result["_timestamp"] = pd.to_datetime("now")
+        self.data = pd.concat([self.data, pd.DataFrame([result])], ignore_index=True)
+        self.data.to_pickle((self._fpath))
+
+    def _print(self, result):
+        for key, value in result.items():
+            if not key.startswith("_"):
+                if type(value) in [int, float, numpy.float64, numpy.float32, numpy.float16, numpy.float128,
+                                   numpy.float256, numpy.int8, numpy.int16, numpy.int32, numpy.int64, numpy.int128]:
+                    print("{}:\t\t{:.2f}".format(key, value))
+                else:
+                    print("{}:\n\t{}".format(key, value))
 
     def __call__(self, fun):
         def wrapped(*args, **kwargs):
             result = fun(*args, **kwargs)
-            result["_timestamp"] = pd.to_datetime("now")
-
-            self.data = pd.concat([self.data, pd.DataFrame([result])], ignore_index=True)
-            self.data.to_pickle(self._fpath.open("w"))
-
-            for key, value in result.items():
-                if not key.startswith("_"):
-                    if type(value) in [int, float, numpy.float64, numpy.float32, numpy.float16, numpy.float128,
-                                       numpy.float256, numpy.int8, numpy.int16, numpy.int32, numpy.int64, numpy.int128]:
-                        print("{}:\t\t{:.2f}".format(key, value))
-                    else:
-                        print("{}:\n\t{}".format(key, value))
+            self._store(result)
+            self._print(result)
 
         return wrapped
