f70c855ed8e9c6f4e22b50827d4020cf5cff5760
Gyorgy Orosz <oroszgy@gmail.com>
2017-06-15 22:24:49 +0200

    Updated experiment tracker

--- mltoolz/experiments.py
+++ mltoolz/experiments.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 
+import numpy
 import pandas as pd
 
 
 class ExperimentTracker(object):
-    def __init__(self, csv_fpath):
-        self._fpath = Path(csv_fpath)
-        if self._fpath.exists():
-            self.data = pd.read_csv(self._fpath.open(), index_col=None, parse_dates=True)
+    def __init__(self, pkl_path, append=True):
+        self._fpath = Path(pkl_path)
+        if self._fpath.exists() and append:
+            self.data = pd.read_pickle(self._fpath.open())
         else:
             self.data = pd.DataFrame()
 
@@ -17,11 +18,12 @@ class ExperimentTracker(object):
             result["_timestamp"] = pd.to_datetime("now")
 
             self.data = pd.concat([self.data, pd.DataFrame([result])], ignore_index=True)
-            self.data.to_csv(self._fpath.open("w"), index=False)
+            self.data.to_pickle(self._fpath.open("w"))
 
             for key, value in result.items():
                 if not key.startswith("_"):
-                    if type(value) in [int, float]:
+                    if type(value) in [int, float, numpy.float64, numpy.float32, numpy.float16, numpy.float128,
+                                       numpy.float256, numpy.int8, numpy.int16, numpy.int32, numpy.int64, numpy.int128]:
                         print("{}:\t\t{:.2f}".format(key, value))
                     else:
                         print("{}:\n\t{}".format(key, value))
